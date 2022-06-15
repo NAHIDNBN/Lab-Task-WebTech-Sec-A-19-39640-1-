2353f7ecdcfc332cb14d54eb680bb0def18667f2
NAHIDNBN <93638525+NAHIDNBN@users.noreply.github.com>
2022-06-16 00:04:34 +0600

    Add files via upload

---
+++ Login page lab task/login1.php
@@ -0,0 +1,6 @@
+<form action="login2.php" method="post">
+Username:<input type="text" id="uname" name="uname"><br><br>
+Password:<input type="password" id="pass" name="pass"><br><br>
+<br>
+<input type="submit" value="Click" name="btnClick">
+</form>
---
+++ Login page lab task/login2.php
@@ -0,0 +1,10 @@
+<?php
+
+$Name= "nahid";
+$Pass= "1";
+if($_POST["uname"]==$Name && $_POST["pass"]==$Pass)
+echo "Welcome";
+else 
+echo "Not found";
+
+?>
