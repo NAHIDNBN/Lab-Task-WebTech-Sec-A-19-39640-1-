52509189a24c2c963f9bb8de79e62fa997931032
NAHIDNBN <93638525+NAHIDNBN@users.noreply.github.com>
2022-07-30 19:47:42 +0600

    Update labTask1final.php

--- labTask1final.php
+++ labTask1final.php
@@ -1,2 +1,25 @@
 <?php 
 $servername = "localhost";
+$username = "root";
+$password = "";
+$dname = "info";
+
+
+$name = "Nahid";
+$dob = "1998-05-02";
+
+$conn = new mysqli($servername, $username, $password, $dname);
+
+
+$q = "insert into Table (Name, DOB) values ('" . $name . "', '" . $dob . "')";
+$result = $conn->query($q);
+
+$q = "update Table set DOB= '" . $dob . "' where name='Rahim' ";
+$result = $conn->query($q);
+
+
+
+$q = "delete from Table where id=2";
+$result = $conn->query($q);
+
+?>
